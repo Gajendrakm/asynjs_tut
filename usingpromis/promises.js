33c2b5f5b12da6cdf4f67d7486dd93f8fcc184ef
Gajendrakm <gjendra@gmail.com>
2021-06-14 00:21:55 +0530

    updated approaches using async

--- usingpromis/promises.js
+++ usingpromis/promises.js
@@ -29,6 +29,43 @@ function createPost(post){
     });
 }
 
-createPost({title:'Post three',body:'This is post three'})
-.then(getPosts)
-.catch(err => console.log(err));
+// createPost({title:'Post three',body:'This is post three'})
+// .then(getPosts)
+// .catch(err => console.log(err));
+
+/* second approach below */
+
+// const promise1 = Promise.resolve('Hello World');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve,reject) =>
+// setTimeout(resolve,2000,'Goodbye')
+// );
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
+// .then(res =>
+// res.json()
+// );
+// Promise.all([promise1,promise2,promise3,promise4])
+// .then(values => console.log(values));
+
+/*thrid approach Async / Await */
+
+// async function init() {
+//     await createPost({title:'Post Three',body: 'This is Post three'})
+
+//     getPosts();
+// }
+
+// init();
+
+
+/**fourth approach */
+
+async function fetchUsers(){
+     const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+     const data = await res.json();
+
+     console.log(data);
+}
+
+fetchUsers();
